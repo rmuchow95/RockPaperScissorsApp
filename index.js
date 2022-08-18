48e3ebeab003e193f3cad808bfd9cade8c56aa81
Rachel Hooker <Rachel@rachels-mbp.lan>
2022-08-18 17:38:04 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const rps = ['rock', 'paper', 'scissors'];
+
+//Click function applied to images//
+
+rock.addEventListener("click", clickRock);
+paper.addEventListener("click", clickPaper);
+scissors.addEventListener("click", clickScissors);
+
+function clickRock() {
+  alert ("The user chose rock");
+}
+function clickPaper() {
+  alert ("The user chose paper");
+}
+function clickScissors() {
+  alert ("The user chose scissors");
+}
+
+
+//get a random number between 0-2
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+const randomNum = getRandomInt(rps.length);
+
+console.log(getRandomInt(rps.length));
