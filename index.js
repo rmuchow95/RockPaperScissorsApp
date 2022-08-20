596ac709784fb9264d969dd0a5d0ce3c67171ff9
Rachel Hooker <Rachel@rachels-mbp.lan>
2022-08-20 12:38:26 -0500

    Used innerHTML to add/remove text with click event; added if/else statements for results

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const rps = ['rock', 'paper', 'scissors'];
+const rockPaperScissors = ['rock', 'paper', 'scissors'];
 
-//Click function applied to images//
+//Click event applied to images//
 
 rock.addEventListener("click", clickRock);
 paper.addEventListener("click", clickPaper);
@@ -8,33 +8,77 @@ scissors.addEventListener("click", clickScissors);
 
 function clickRock() {
   const paragraphElement = document.getElementById("response");
+  paragraphElement.innerHTML = "";
   const node = document.createTextNode("The user chose rock");
   paragraphElement.appendChild(node);
-  const node1 = document.createTextNode("The computer chose " + computerChoice);
-  paragraphElement.appendChild(node1);
+  let randomNum = getRandomInt(rockPaperScissors.length);
+  const resultElement = document.getElementById("result");
+  resultElement.innerHTML = "";
+  const node1 = document.createTextNode("The computer chose " + rockPaperScissors[randomNum]);
+  resultElement.appendChild(node1);
+
+  if (randomNum == 0) {
+    console.log("It was a tie!");
+  } else if (randomNum == 1) {
+    console.log("The computer won!");
+  } else if (randomNum == 2) {
+    console.log("The user won!");
+  }
 }
+
 function clickPaper() {
   const paragraphElement = document.getElementById("response");
+  paragraphElement.innerHTML = "";
   const node = document.createTextNode("The user chose paper");
   paragraphElement.appendChild(node);
-  const node1 = document.createTextNode("The computer chose " + computerChoice);
-  paragraphElement.appendChild(node1);
+  let randomNum = getRandomInt(rockPaperScissors.length);
+  const resultElement = document.getElementById("result");
+  resultElement.innerHTML = "";
+  const node1 = document.createTextNode("The computer chose " + rockPaperScissors[randomNum]);
+  resultElement.appendChild(node1);
+
+  if (randomNum == 0) {
+    console.log("The user won!");
+  } else if (randomNum == 1) {
+    console.log("It was a tie!");
+  } else if (randomNum == 2) {
+    console.log("The computer won!");
+  }
 }
+
 function clickScissors() {
   const paragraphElement = document.getElementById("response");
+  paragraphElement.innerHTML = "";
   const node = document.createTextNode("The user chose scissors");
   paragraphElement.appendChild(node);
-  const node1 = document.createTextNode("The computer chose " + computerChoice);
-  paragraphElement.appendChild(node1);
-}
+  let randomNum = getRandomInt(rockPaperScissors.length);
+  const resultElement = document.getElementById("result");
+  resultElement.innerHTML = "";
+  const node1 = document.createTextNode("The computer chose " + rockPaperScissors[randomNum]);
+  resultElement.appendChild(node1);
 
+  if (randomNum == 0) {
+    console.log("The computer won!");
+  } else if (randomNum == 1) {
+    console.log("The user won!");
+  } else if (randomNum == 2) {
+    console.log("It was a tie!");
+  }
 
-//get a random number between 0-2
+  }
+
+//Computer's choice selected upon click event//
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
-const randomNum = getRandomInt(rps.length);
-const computerChoice = rps[randomNum];
 
-
-console.log('The computer chose', computerChoice);
+function printResults() {
+  switch (computerChoice) {
+    case "0":
+      console.log("It was a tie!");
+    case "1":
+      console.log("The computer won!");
+    case "2":
+      console.log("The user won!");
+  }
+}
